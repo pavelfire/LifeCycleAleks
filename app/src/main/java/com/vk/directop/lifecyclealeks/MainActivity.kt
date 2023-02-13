7786e658c9f18abdcb47b66f91d0ef120d5bf762
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-13 12:11:48 +0300

    receving data from Lesson1Activity.kt

--- app/src/main/java/com/vk/directop/lifecyclealeks/MainActivity.kt
+++ app/src/main/java/com/vk/directop/lifecyclealeks/MainActivity.kt
@@ -4,16 +4,22 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 
 
- const val KEY_ONE = "HELLO_KEY"
- const val RESULT_KEY = "RESULT_KEY"
+const val KEY_ONE = "HELLO_KEY"
+const val RESULT_KEY = "RESULT_KEY"
+private const val KEY_TEXT_RECEIVED = "KEY_TEXT_RECEIVED"
+private const val TAG = "LifeCycleAleks"
+private var receivedText = "init"
 
 class MainActivity : AppCompatActivity() {
+
+
 
     private val startForResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
@@ -26,6 +32,7 @@ class MainActivity : AppCompatActivity() {
                         intent.extras?.getString(RESULT_KEY).toString()
                     if (received != "") {
                         tvInfo1.text = received
+                        receivedText = received
                     }
                 }
             }
@@ -37,6 +44,20 @@ class MainActivity : AppCompatActivity() {
 
         val btLesson1 = findViewById<Button>(R.id.btLesson1)
         val tvInfo1 = findViewById<TextView>(R.id.tvInfo1)
+        val btLesson2 = findViewById<Button>(R.id.btLesson2)
+
+        receivedText = tvInfo1.text.toString()
+
+        if (savedInstanceState != null){
+            receivedText = savedInstanceState.getString(KEY_TEXT_RECEIVED, "up")
+            Log.d(TAG, "get SaveInstanceState $receivedText")
+            tvInfo1.text = receivedText
+        }
+
+        btLesson2.setOnClickListener {
+            tvInfo1.text = receivedText
+        }
+
 
         btLesson1.setOnClickListener {
             val intent1 = Intent(this, Lesson1Activity::class.java)
@@ -46,5 +67,11 @@ class MainActivity : AppCompatActivity() {
             )
             startForResult.launch(intent1)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState Called $receivedText")
+        outState.putString(KEY_TEXT_RECEIVED, receivedText)
     }
 }
